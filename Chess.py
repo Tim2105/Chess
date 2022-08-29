3fb79562626253d7c43d16e4455a562a6d626e8c
Tim Plotzki <plotzki.tim@fh-swf.de>
2022-08-29 03:41:45 +0200

    NegaScout sucht ab einer früheren Tiefe mit einem Nullfenster -> riesiger Leistungsboost

--- Chess.py
+++ Chess.py
@@ -12,7 +12,7 @@ def pick_move(board : Board) -> Board.Move:
 WHITE = 0
 BLACK = 1
 
-starting_board = Board.Board("8/1k6/1p6/8/2P5/PK6/8/8 w - - 0 1")
+starting_board = Board.Board()
 
 cp = ChessComputer.ChessComputer()
 
